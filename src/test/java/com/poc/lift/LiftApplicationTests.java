c06ac3e7cf1cf2f8ce9cecaeef147d6d60a2f142
Bryan Escobar <bescobarres@gmail.com>
2023-03-30 18:19:25 +1030

    Initial base project

---
+++ src/test/java/com/poc/lift/LiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.lift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
